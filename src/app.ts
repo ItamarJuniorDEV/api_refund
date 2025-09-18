709423ccfe541809a990715fada38eb75dc5801a
Itamar Junior <itamar.junior@irriga.net>
2025-09-18 19:41:29 -0300

    feat: criar tratamento de erros.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+import { errorHandling } from "./middlewares/error-hadling";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -8,5 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.use(errorHandling);
 
 export { app }
